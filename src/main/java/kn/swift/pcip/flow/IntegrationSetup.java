3cb861c668f8d8af6b8bf7180c6f76e897d17833
Glen.McCormick <Glen.McCormick@kuehne-nagel.com>
2019-03-18 14:21:12 +0000

    Added HTTP Endpoint

--- src/main/java/kn/swift/pcip/flow/IntegrationSetup.java
+++ src/main/java/kn/swift/pcip/flow/IntegrationSetup.java
@@ -2,22 +2,26 @@ package kn.swift.pcip.flow;
 
 import kn.swift.pcip.configuration.properties.WmsProperties;
 import kn.swift.pcip.service.PcipHandler;
-import kn.swift.pcip.service.Transformer;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.MessageChannels;
 import org.springframework.integration.dsl.context.IntegrationFlowContext;
+import org.springframework.integration.http.dsl.Http;
 import org.springframework.integration.jms.dsl.Jms;
+import org.springframework.messaging.MessageChannel;
 
 import javax.annotation.PostConstruct;
 
 
 @Configuration
 @EnableIntegration
-public class Config {
+public class IntegrationSetup {
 
 
 
@@ -26,7 +30,7 @@ public class Config {
     private  final WmsProperties wmsProperties;
 
     @Autowired
-    public Config (IntegrationFlowContext integrationFlowContext, PcipHandler handler, WmsProperties wmsProperties){
+    public IntegrationSetup(IntegrationFlowContext integrationFlowContext, PcipHandler handler, WmsProperties wmsProperties){
         this.integrationFlowContext = integrationFlowContext;
         this.handler = handler;
         this.wmsProperties = wmsProperties;
@@ -34,11 +38,20 @@ public class Config {
 
 
     @PostConstruct
-    @Autowired
     public void flows(){
         this.integrationFlowContext.registration(amqInboundFlow(wmsProperties.getJms().getBroker(), wmsProperties.getJms().getDestination())).register();
+        this.integrationFlowContext.registration(httpInboundFlow("/pcip")).register();
     }
 
+    private IntegrationFlow httpInboundFlow(String endpoint){
+        return IntegrationFlows.from(Http.inboundGateway(endpoint)
+                .requestMapping(m -> m.methods(HttpMethod.POST))
+                .requestPayloadType(String.class).replyChannel(directChannel())
+        )
+                .channel("httpRequest")
+                .handle(handler, "handle")
+                .get();
+    }
 
     // TODO: Handle errors - this appears transactional and retries 'X' times - errorChannel()?
     private IntegrationFlow amqInboundFlow(String broker, String queue){
@@ -46,5 +59,10 @@ public class Config {
                 .destination(queue))
                 .handle(handler, "handle")
                 .get();
+    }
+
+    @Bean
+    public MessageChannel directChannel(){
+        return MessageChannels.direct().get();
     }
 }
